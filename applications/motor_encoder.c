222c81dfeec42305641a1cd9407f3f49a1e5ca08
xym <565000561@qq.com>
2023-09-27 23:08:30 +0800

    Update motor_encoder.c, motor_encoder.h, and 2 more files...

--- applications/motor_encoder.c
+++ applications/motor_encoder.c
@@ -5,7 +5,33 @@
 
 
 
-static int pulse_encoder_init(void)
+static rt_int32_t trans(rt_int32_t c, rt_uint8_t m)
+{
+    rt_int32_t temp;
+    
+    if (c > 32768/2)
+    {
+        temp = c - 32768;    
+    }
+    else
+    {
+        temp = c;
+    }
+    
+    if (m == 1)
+    {
+        temp = -temp;
+    }
+
+    return temp;
+}
+
+
+
+static rt_thread_t encoder_thread = RT_NULL;
+
+
+static void encoder_thread_entry(void *parameter)
 {
     rt_err_t ret = RT_EOK;
     
@@ -34,29 +60,57 @@ static int pulse_encoder_init(void)
     if (ret != RT_EOK)
     {
         rt_kprintf("open encoder device failed!\n");
-        return ret;
     }
+    
+    
+    rt_int32_t count_lf, count_lb, count_rf, count_rb;
+    
     
     while(1)
     {
         rt_thread_mdelay(500);
         
-        /* 读取脉冲编码器计数值 */
+        
         rt_device_read(pulse_encoder_dev1, 0, &count, 1);
-        rt_kprintf("%6d ",count);
+        count_lf = trans(count, 1);
+        
         rt_device_read(pulse_encoder_dev2, 0, &count, 1);
-        rt_kprintf("%6d ",count);
+        count_lb = trans(count, 1);
+        
         rt_device_read(pulse_encoder_dev3, 0, &count, 1);
-        rt_kprintf("%6d ",count);        
+        count_rf = trans(count, 1);        
+        
         rt_device_read(pulse_encoder_dev4, 0, &count, 1);
-        rt_kprintf("%6d \n",count);
+        count_rb = trans(count, 1);
+
+        
+        /* 读取脉冲编码器计数值 */
+        rt_kprintf("%6d ", count_lf);
+        rt_kprintf("%6d ", count_lb);
+        rt_kprintf("%6d ", count_rf);        
+        rt_kprintf("%6d \n", count_rb);
     }
 }
 
-/* 导出到 msh 命令列表中 */
-MSH_CMD_EXPORT(pulse_encoder_init, pulse encoder sample);
+int encoder_thread_init(void)
+{
+    rt_err_t result = RT_EOK;
 
+    /* 创建线程 1，名称是 thread1，入口是 thread1_entry*/
+    encoder_thread = rt_thread_create("encoder",
+                            encoder_thread_entry, 
+                            RT_NULL,
+                            ENCODER_STACK_SIZE,
+                            ENCODER_PRIORITY, 
+                            ENCODER_TIMESLICE);
 
+    /* 如果获得线程控制块，启动这个线程 */
+    if (encoder_thread != RT_NULL)
+        rt_thread_startup(encoder_thread);
 
+    return 0;
+}
+
+INIT_APP_EXPORT(encoder_thread_init);
 
 
--- applications/motor_encoder.h
+++ applications/motor_encoder.h
@@ -9,13 +9,13 @@
 #define ENCODER_NAME_LF     "pulse3"
 #define ENCODER_NAME_LB     "pulse2"    /* 脉冲编码器名称 */
 #define ENCODER_NAME_RF     "pulse4"    /* 脉冲编码器名称 */
-
 #define ENCODER_NAME_RB     "pulse5"    /* 脉冲编码器名称 */
 
 
 
-
-
+#define     ENCODER_STACK_SIZE      1024
+#define     ENCODER_PRIORITY        5
+#define     ENCODER_TIMESLICE       10
 
 
 
